a41f78a6002fc6923da303a8a2522c14162c9e7f
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-11-20 18:06:15 +0800

    Update base.py

--- genmod/vanilla_gan/base.py
+++ genmod/vanilla_gan/base.py
@@ -1,186 +1,103 @@
-import abc
 import tensorflow as tf
-from tfutils.monte_carlo_integral import (
-    MonteCarloIntegral, monte_carlo_integrate)
-from tfutils.graph import get_dependent_variables
+from tfutils.monte_carlo_integral import monte_carlo_integrate
+from genmod.f_gan.base import BaseFGAN
+from genmod.f_gan.f_divergence import BaseFDivergence
 
 
-class GANLoss(object):
+def log_sigmoid(x):
+  r"""Returns `ln[ sigmoid(x) ]`.
 
-  def __init__(self, discriminator, generator):
-    self.discriminator = discriminator
-    self.generator = generator
-
-
-class BaseVanillaGAN(abc.ABC):
-  r"""
   ```math
-  Let $\mathcal{X}$ the space of data (ambient-space), and $\mathcal{Z}$
-  the latent, equipped with measurement $P_Z$ (prior). Discriminator
+  Denote sigmoid-function by $\sigma$ and softplus-function by $s$, then
 
-  $$D: \mathcal{X} \mapsto \mathbb{R}.$$
+  \begin{equation}
+    \ln \sigma(x) = -s(-x).
+  \end{equation}
+  ```
+  """
+  return -tf.nn.softplus(-x)
 
-  And generator
 
-  $$G: \mathcal{Z} \mapsto \mathcal{X}.$$
+def log1m_sigmoid(x):
+  r"""Returns `ln[ 1 - sigmoid(x) ]`.
 
-  The loss is defined as
+  ```math
+  Denote sigmoid-function by $\sigma$ and softplus-function by $s$, then
 
-  \begin{equatin}
+  \begin{equation}
+    \ln \left( 1 - \sigma(x) \right) = -s(x).
+  \end{equation}
+  ```
+  """
+  return -tf.nn.softplus(x)
 
-    L[D, G] :=
-      + \mathbb{E}_{x \sim P_{\textrm{data}}} \left[ \ln \sigma(D(x)) \right]
-      + \mathbb{E}_{z \sim P_{Z}} \left[
-            \ln \left( 1 - \sigma(D(G(z))) \right)
-        \right].
 
+class GANDivergence(BaseFDivergence):
+  r"""Follows the "Alternative M-step #2" in reference [1].
+
+  ```math
+  Denote sigmoid function by $\sigma$ and softplus by $s$. GAN-divergence
+  (inheriting f-divergence) is defined by setting
+
+  \begin{align}
+    g_f(x) := \ln \sigma(x);
+    f^{\*}(x) := x - \ln \left( 1 - e^x \right).
+  \end{align}
+
+  Denote the discriminator as $D: \mathcal{X} \mapsto \mathbb{R}$, and
+  $V := \sigma \circ D, \mathcal{X} \mapsto (0, 1)$. Then, by the relations
+
+  \begin{align}
+    \ln \sigma(x) = -s(-x);
+    \ln \left[ 1 - \sigma(x) \right] = -s(x),
+  \end{align}
+
+  the GAN-divergence can be reduced to
+
+  \begin{equation}
+    D_{\textrm{GAN}} \left[ P \| Q \right]:=
+      \mathbb{E}_{x \sim P} \left[ \ln V(x) \right] -
+      \mathbb{E}_{x \sim Q} \left[ \ln \frac{ V(x) }{ 1 - V(x) } \right],
   \end{equation}
 
-  Maximizing $L$ when training $D$ and minimizing when training $G$.
-
-  However, for numerical efficiency and stability, the loss when training $G$
-  is re-defined as
-
-  \begin{equatin}
-
-    L[G \mid D] :=
-      - \mathbb{E}_{z \sim P_{Z}} \left[
-            \ln \sigma(D(G(z))) -
-            \ln \left( 1 - \sigma(D(G(z))) \right)
-        \right],
-
-  \end{equation}
-
-  instead, for any $D$ given. This re-definition keeps the global miminum
-  invariant.
+  wherein the first term is the "discriminate part" and the second the
+  "generate part".
   ```
 
-  Notations:
-    A: Event-shape of the ambient.
-    L: Event-shape of the latent.
+  References:
+    1. https://www.inference.vc/an-alternative-update-rule-for-generative-adversarial-networks/  # noqa: E501
 
   Args:
-    n_samples: Positive integer. Shall be greater than 30 as a thumb-rule
-      for central limit theorem, employed by the Monte-Carlo integral
-      (i.e. the E_{z~Pz}[...]).
+    n_samples: Positive integer.
+    name: String.
   """
 
-  def __init__(self, n_samples=32):
+  def __init__(self, n_samples, name='GAN_divergence'):
+    super().__init__(n_samples=n_samples, name=name)
+
+  @staticmethod
+  def output_activation(x):
+    return log_sigmoid(x)
+
+  @staticmethod
+  def f_star(x):
+    return x - tf.log1p(-tf.exp(x))
+
+  def generate_part(self, fake_data, discrimator, reuse):
+    """Re-implementing for avoiding the numerical instability caused by
+    the `tf.exp` in `self.f_star`."""
+    discr_fake = discrimator(fake_data, reuse)
+    integrands = -log_sigmoid(discr_fake) + log1m_sigmoid(discr_fake)
+    return monte_carlo_integrate(integrands, axes=[0])
+
+
+class BaseVanillaGAN(BaseFGAN):
+
+  def __init__(self, n_samples=32, **kwargs):
+    super().__init__(**kwargs)
     self.n_samples = n_samples
-
-    self._generator_vars = None
-    self._discriminator_vars = None
-
-  def loss(self,
-           data,
-           name='GAN_loss',
-           reuse=tf.AUTO_REUSE):
-    r"""Follows the "Alternative M-step #2" in reference [1].
-
-    ```math
-    Denote sigmoid function by $\sigma$ and softplus by $s$. Useful relations:
-
-    \begin{align}
-
-      \ln \sigma(x) = -s(-x);
-
-      \ln \left[ 1 - \sigma(x) \right] = -softplus(x).
-
-    \end{align}
-    ```
-
-    References:
-      1. https://www.inference.vc/an-alternative-update-rule-for-generative-adversarial-networks/  # noqa: E501
-
-    Args:
-      data:
-      name: String.
-      reuse: Boolean.
-    """
-    with tf.name_scope(name):
-      discr_real = self.discriminator(data, reuse)
-      fake_data = self._generator(self.n_samples, reuse)
-      discr_fake = self.discriminator(fake_data, reuse)
-
-      self._discriminator_vars = get_dependent_variables(discr_real)
-      self._generator_vars = get_dependent_variables(fake_data)
-
-      with tf.name_scope('discriminator_loss'):
-        discr_loss = MonteCarloIntegral(
-            value=tf.constant(0, dtype=data.dtype),
-            error=tf.constant(0, dtype=data.dtype))
-        for integrands in (tf.nn.softplus(-discr_real),
-                           tf.nn.softplus(discr_fake)):
-          mc_int = monte_carlo_integrate(integrands, axes=[0])
-          discr_loss.value += mc_int.value
-          discr_loss.error += mc_int.error
-
-      with tf.name_scope('generator_loss'):
-        # gen_loss_integrand = (tf.nn.softplus(-discr_fake) -
-        #                       tf.nn.softplus(discr_fake))
-        gen_loss_integrand = tf.nn.softplus(-discr_fake)  # test!
-        gen_loss = monte_carlo_integrate(gen_loss_integrand)
-
-      return GANLoss(discriminator=discr_loss,
-                     generator=gen_loss)
-
-  @abc.abstractmethod
-  def discriminator(self, ambient, reuse):
-    """
-    Args:
-      ambient: Tensor with shape `[B] + A`, for arbitrary positive integer
-        `B`.
-      reuse: Boolean.
-
-    Returns:
-      Tensor with shape `[B1]`.
-    """
-    pass
+    self.gan_divergence = GANDivergence(self.n_samples)
 
   @property
-  @abc.abstractmethod
-  def prior(self):
-    """The prior distribution of the latent.
-
-    Returns:
-      Distribution with event-shape `L`.
-    """
-    pass
-
-  @abc.abstractmethod
-  def generator(self, latent, reuse):
-    """The function `g` that gives X = g(Z), where X the ambient and Z
-    the latent.
-
-    Args:
-      latent: Tensor with shape `[B] + L`, for arbitrary positive integer `B`.
-      reuse: Boolean.
-
-    Returns:
-      Tensor with shape `[B] + A`.
-    """
-    pass
-
-  def _generator(self, n_samples, reuse):
-    """
-    Args:
-      n_samples: Positive integer.
-      reuse: Boolean.
-
-    Returns:
-      Tensor with shape `[n_samples] + A`.
-    """
-    with tf.name_scope('generator'):
-      # [n_samples] + L
-      latent_samples = self.prior.sample(n_samples)
-      # [n_samples] + A
-      ambient_samples = self.generator(latent_samples, reuse)
-      return ambient_samples
-
-  @property
-  def discriminator_vars(self):
-    return self._discriminator_vars
-
-  @property
-  def generator_vars(self):
-    return self._generator_vars
+  def f_divergence(self):
+    return self.gan_divergence
